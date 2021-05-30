9283d925f42809f4389b4738d8e090f8b04e1ff1
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-30 15:39:51 +0300

    add login functionalities

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .decorators import unauthenticated_user
+from django.contrib import messages
 
 # Create your views here.
+@unauthenticated_user
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+    context = {}
+    return render(request, 'registration/login.html', context)
