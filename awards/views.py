5af57c4110c95ffa62d086a1790b9c85e55a300b
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-30 17:10:12 +0300

    add logout and profile edit functions

--- awards/views.py
+++ awards/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .decorators import unauthenticated_user
 from django.contrib import messages
-from .forms import  CreateUserForm
+from .forms import  CreateUserForm, ProfileForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
 @unauthenticated_user
 def loginPage(request):
     if request.method == 'POST':
@@ -29,11 +33,28 @@ def registerPage(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
-            # Profile.objects.create(
-            #     user=user,
-            # )
+            Profile.objects.create(
+                user=user,
+            )
             # send_welcome_email(user.username,user.email)
             messages.success(request, 'Account was creates for ' + username )
             return redirect('login')
     context = {'form':form}
     return render(request, 'registration/register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+def accountSettings(request):
+	profile = request.user.profile
+	form = ProfileForm(instance=profile)
+	if request.method == 'POST':
+		form = ProfileForm(request.POST, request.FILES,instance=profile)
+		if form.is_valid():
+			form.save()
+
+
+	context = {'form':form}
+	return render(request, 'profile/profile_edit.html', context)
